0167b938cd2963ed214245e6065a68182f0a7694
pulidxxx <andresfelipepulidosuarez@gmail.com>
2025-11-22 00:01:22 -0500

    [TEST] for deploy in vercel

---
+++ api/index.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from '../src/app.module';
+import { ClassSerializerInterceptor } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import express from 'express';
+
+const server = express();
+
+export default async (req, res) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+  );
+  
+  app.enableCors();
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  
+  await app.init();
+  
+  return server(req, res);
+};
